fefdd1818bbcd8796482fdd4b4fcc09334f2fc0d
java <bedwani@yahoo.com>
2013-07-22 17:30:45 +0200

    krager algorithm min cut final

--- minCut/src/mincut/MinCut.java
+++ minCut/src/mincut/MinCut.java
@@ -4,9 +4,15 @@
  */
 package mincut;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  *
@@ -17,171 +23,198 @@ public class MinCut {
     /**
      * @param args the command line arguments
      */
-
-    List<Edge> edgeList=new LinkedList<>();
-    List<Node> vertexList=new LinkedList<>();
+    List<Edge> edgeList = new LinkedList<>();
+    List<Node> vertexList = new LinkedList<>();
     UF uf;
-    Random randomGenerator=new Random();
-    
-    public MinCut(int n, int m,List v, List e ) // n # of vertices , m # of edges
+    Random randomGenerator = new Random();
+
+    public MinCut(int n, int m, List v, List e) // n # of vertices , m # of edges
     {
-        uf=new UF(n);
-        edgeList=e;
-        vertexList=v;
+        uf = new UF(n);
+        edgeList = e;
+        vertexList = v;
+        // remove the duplicate links on the list
+        int[] temp = new int[edgeList.size() / 2];
+        int counter = 0;
+        for (int i = 0; i < edgeList.size(); i++) {
+
+            int tailindex = edgeList.get(i).getTail().getId();
+            int headindex = edgeList.get(i).getHead().getId();
+            int j = i + 1;
+            boolean found = false;
+            while (found == false && j < edgeList.size()) {
+                if ((tailindex == edgeList.get(j).getHead().getId()) && (headindex == edgeList.get(j).getTail().getId())) {
+                    temp[counter] = j;
+                    counter = counter + 1;
+                    found = true;
+                    edgeList.remove(j);
+
+                } else {
+                    j = j + 1;
+                }
+            }
+        }
+
     }
-    public int minCut()
-    {
+
+    public int minCut() {
         int index;
         int tailId, headId;
-        
-        while (vertexList.size()>2)
-        {
-           if (edgeList.size()>0){
-            index=getRandom(edgeList.size());
-            tailId=edgeList.get(index).getTail().getId();
-            headId=edgeList.get(index).getHead().getId();
-            uf.union(tailId, headId);
-           
-            removeVertex(edgeList.get(index).getTail(),edgeList.get(index).getHead());
-            removeSelfLoops();
-            
-        }
+
+        while (vertexList.size() > 2 && !edgeList.isEmpty()) {
+            if (edgeList.size() > 0) {
+                index = getRandom(edgeList.size());
+                tailId = edgeList.get(index).getTail().getId();
+                headId = edgeList.get(index).getHead().getId();
+                uf.union(tailId, headId);
+                edgeList.remove(index);
+                removeVertex(tailId, headId);
+                AdjustEdges(tailId, headId);
+                removeSelfLoops();
+            }
         }
         return edgeList.size();
     }
-    private void removeSelfLoops()
-    {
-        int t, h;
-        int[] temp=new int[edgeList.size()];
-        int j=0;
-        
-        for (int i=0; i<edgeList.size();i++)
-        {
-            t=edgeList.get(i).getTail().getId();
-            h=edgeList.get(i).getHead().getId();
-            if (uf.connected(t, h)) {
-                temp[j]=i;
-                j=j+1;          
+
+    private void AdjustEdges(int tailId, int headId) {
+        int tochange = 0;
+        int changeto = 0;
+        if (tailId >= headId) {
+            tochange = tailId;
+            changeto = headId;
+        } else {
+            tochange = headId;
+            changeto = tailId;
+        }
+        Node temp;
+        for (int i = 0; i < edgeList.size(); i++) {
+            if (edgeList.get(i).getTail().getId() == tochange) {
+                temp = new Node();
+                temp.setId(changeto);
+                edgeList.get(i).setTail(temp);
+            } else {
+                if (edgeList.get(i).getHead().getId() == tochange) {
+                    temp = new Node();
+                    temp.setId(changeto);
+                    edgeList.get(i).setHead(temp);
+                }
             }
         }
-       for (int i=j-1; i>=0;i--)
-                    edgeList.remove(temp[i]);
-    
+
     }
-    
-    
-    private void removeVertex(Node t,Node h)
-    {
-        //if (vertexList.contains(t)) {
-        //    vertexList.remove(t);
-        //}
-        
-        if (vertexList.contains(t)) {
-            vertexList.remove(t);
-           
-        }
-        else
-            if (vertexList.contains(h)){
-                vertexList.remove(h);
+
+    private void removeSelfLoops() {
+        int[] temp = new int[edgeList.size()];
+        int count = 0;
+        int t, h;
+        for (int i = 0; i < edgeList.size(); i++) {
+            t = edgeList.get(i).getTail().getId();
+            h = edgeList.get(i).getHead().getId();
+
+            if (uf.connected(t, h)) {
+                temp[count] = i;
+                count += 1;
             }
-     }
-    
-    
-    
+
+
+        }
+        for (int i = count - 1; i >= 0; i--) {
+            edgeList.remove(temp[i]);
+        }
+    }
+
+    private void removeVertex(int t, int h) {
+
+        for (int i = vertexList.size() - 1; i >= 0; i--) {
+            if (t == vertexList.get(i).getId()) {
+                vertexList.remove(i);
+                break;
+            } else {
+                if (h == vertexList.get(i).getId()) {
+                    vertexList.remove(i);
+                    break;
+                }
+            }
+        }
+    }
+
     private int getRandom(int m) // m is the # of remaining Edges in Edges list
     {
-            
-            return randomGenerator.nextInt(m);
+
+        return randomGenerator.nextInt(m);
     }
-    
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws FileNotFoundException {
         // TODO code application logic here
-  
-        
-    List<Node> v=new LinkedList<>();
-    List<Edge> e=new LinkedList<>();
-    
-    Node[] temp=new Node[8];
-    
-    for (int i=0; i<8;i++)
-    {
-        temp[i]=new Node();
-        temp[i].setId(i);
-        temp[i].setNext(null);
-        v.add(temp[i]);
-    }
-    //
-    
-    Edge[] t=new Edge[14];
-    
-        for (int i=0; i<14; i++)
-        {
-            t[i]=new Edge();
-            t[i].setDirected(false);
-            
-        }
-        t[0].setTail(temp[0]);
-        t[0].setHead(temp[1]);
-        t[1].setTail(temp[0]);
-        t[1].setHead(temp[2]);
-        t[2].setTail(temp[0]);
-        t[2].setHead(temp[3]);
-        t[3].setTail(temp[0]);
-        t[3].setHead(temp[3]);// changed from 6 for testing
-        
-        //t[2].setTail(temp[1]);
-        //t[2].setHead(temp[0]);
-        t[4].setTail(temp[1]);
-        t[4].setHead(temp[2]);
-        t[5].setTail(temp[1]);
-        t[5].setHead(temp[3]);
-        
-        t[6].setTail(temp[2]);
-        t[6].setHead(temp[3]);
-        //t[4].setTail(temp[2]);
-        //t[4].setHead(temp[0]);
-        //t[5].setTail(temp[2]);
-        //t[5].setHead(temp[1]);
-        t[7].setTail(temp[3]);
-        t[7].setHead(temp[4]);
-        t[8].setTail(temp[4]);
-        t[8].setHead(temp[5]);
-        t[9].setTail(temp[4]);
-        t[9].setHead(temp[6]);
-        t[10].setTail(temp[5]);
-        t[10].setHead(temp[6]);
-        t[11].setTail(temp[5]);
-        t[11].setHead(temp[7]);
-        t[12].setTail(temp[6]);
-        t[12].setHead(temp[7]);
-        t[13].setTail(temp[4]);
-        t[13].setHead(temp[7]);
-        
-        
-        
-    for (int i=0; i<14; i++)
-        {
-           e.add(t[i]);
-            
-        }
-        
-         
-        
-        MinCut mc=new MinCut(8,14,v,e);
 
-        int result=mc.minCut();
-        System.out.println("# of edges"+result);
-        
-        System.out.println(mc.vertexList.get(0).getId());
-        System.out.println(mc.vertexList.get(1).getId());
-        
-        for (int i=0; i<mc.edgeList.size();i++)
-        {
-            System.out.print(mc.edgeList.get(i).getTail().getId());
-            System.out.print("     ");
-            System.out.print(mc.edgeList.get(i).getHead().getId());
-            System.out.println();
-        }
-     }
-    }
 
+        List<Node> v = new LinkedList<>();
+        List<Edge> e = new LinkedList<>();
+
+        Node[] temp = new Node[200];
+        for (int iteration = 0; iteration < 500; iteration++) {
+            v = new LinkedList<>();
+            e = new LinkedList<>();
+
+            int counter = 0;
+            File file = new File("c:\\kargerMinCut.txt");
+            Scanner s;
+            BufferedReader bf = new BufferedReader(new FileReader(file));
+            Node edgetemp;
+            Edge[] t = new Edge[5034];
+
+            counter = 0;
+
+            String line = "";
+            Scanner lineScanner;
+            s = new Scanner(file);
+            for (int i = 0; i < 200; i++) {
+                line = s.nextLine();
+                lineScanner = new Scanner(line);
+                temp[i] = new Node();
+                temp[i].setId(lineScanner.nextInt() - 1);
+                temp[i].setNext(null);
+                v.add(temp[i]);
+                while (lineScanner.hasNextInt()) {
+                    t[counter] = new Edge();
+                    t[counter].setDirected(false);
+                    edgetemp = new Node();
+                    edgetemp.setId(temp[i].getId());
+                    t[counter].setTail(edgetemp);
+                    edgetemp = new Node();
+                    edgetemp.setId(lineScanner.nextInt() - 1);
+                    edgetemp.setNext(null);
+                    t[counter].setHead(edgetemp);
+                    counter += 1;
+                }
+                lineScanner.close();
+
+            }
+
+
+
+            for (int i = 0; i < counter; i++) {
+                e.add(t[i]);
+
+            }
+
+
+
+            MinCut mc = new MinCut(200, counter, v, e);
+
+            int result = mc.minCut();
+            System.out.println("# of edges  " + result);
+
+            //System.out.println(mc.vertexList.get(0).getId());
+            //System.out.println(mc.vertexList.get(1).getId());
+
+            //for (int i=0; i<mc.edgeList.size();i++)
+            // {
+            //   System.out.print(mc.edgeList.get(i).getTail().getId());
+            //   System.out.print("     ");
+            //   System.out.print(mc.edgeList.get(i).getHead().getId());
+            //   System.out.println();
+            s.close();
+        }
+    }
+}
